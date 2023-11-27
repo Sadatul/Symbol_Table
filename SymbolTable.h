b9314086ba2a692e1fd837ec2d0324eaeb6208fb
Sadatul <sadatulislamsadi@gmail.com>
2023-11-27 19:21:19 +0600

    Added ScopeTable class

--- SymbolTable.h
+++ SymbolTable.h
@@ -49,4 +49,164 @@ public:
 
 class ScopeTable
 {
+private:
+    unsigned long hash(const string &str)
+    {
+        // Using sdbm hash
+        unsigned long hash = 0;
+        for (char c : str)
+        {
+            hash = c + (hash << 6) + (hash << 16) - hash;
+        }
+        return hash % totalBuckets;
+    }
+
+    void deleteRecur(SymbolInfo *node)
+    {
+        if (node == NULL)
+            return;
+
+        deleteRecur(node->getNext());
+        delete (node);
+    }
+
+    int totalBuckets;
+    string id;
+    ScopeTable *parentScope;
+    SymbolInfo **table;
+
+public:
+    ScopeTable(string id, int totalBuckets)
+    {
+        this->id = id;
+        this->totalBuckets = totalBuckets;
+        table = new SymbolInfo *[totalBuckets];
+        for (int i = 0; i < totalBuckets; i++)
+        {
+            table[i] = NULL;
+        }
+    }
+
+    ~ScopeTable()
+    {
+        for (int i = 0; i < totalBuckets; i++)
+        {
+            deleteRecur(table[i]);
+        }
+
+        delete[] table;
+    }
+    string getId()
+    {
+        return id;
+    }
+
+    // void setId(string id){
+    //     this->id = id;
+    // }
+
+    int getTotalBuckets()
+    {
+        return totalBuckets;
+    }
+
+    ScopeTable *getParentScope()
+    {
+        return parentScope;
+    }
+
+    void setParentScope(ScopeTable *p)
+    {
+        this->parentScope = p;
+    }
+
+    SymbolInfo *lookUp(string name)
+    {
+        int index = hash(name); // %totalBuckets is being done inside the hash function
+        SymbolInfo *tmp = table[index];
+        while (tmp)
+        {
+            if (tmp->getName() == name)
+            {
+                return tmp;
+            }
+
+            tmp = tmp->getNext();
+        }
+
+        return NULL;
+    }
+
+    bool insert(string name, string type)
+    {
+        int index = hash(name); // %totalBuckets is being done inside the hash function
+        if (table[index] == NULL)
+        {
+            table[index] = new SymbolInfo(name, type);
+            return true;
+        }
+
+        SymbolInfo *tmp = table[index];
+        while (true)
+        {
+            if (tmp->getName() == name)
+            {
+                return false;
+            }
+            if (tmp->getNext() == NULL)
+            {
+                tmp->setNext(new SymbolInfo(name, type));
+                return true;
+            }
+            tmp = tmp->getNext();
+        }
+    }
+
+    bool Delete(string name)
+    {
+        int index = hash(name); // %totalBuckets is being done inside the hash function
+        if (table[index] == NULL)
+        {
+            return false;
+        }
+        if (table[index]->getName() == name)
+        {
+            SymbolInfo *tmp = table[index];
+            table[index] = tmp->getNext();
+            delete tmp;
+            return true;
+        }
+
+        SymbolInfo *parent = table[index];
+
+        while (SymbolInfo *child = parent->getNext())
+        {
+            if (child->getName() == name)
+            {
+                parent->setNext(child->getNext());
+                delete child;
+                return true;
+            }
+            parent = child;
+        }
+
+        return false;
+    }
+
+    void print()
+    {
+        cout << "\tScopeTable# " << id << " created" << endl;
+        for (int i = 0; i < totalBuckets; i++)
+        {
+            cout << "\t" << i + 1;
+
+            SymbolInfo *tmp = table[i];
+            while (tmp)
+            {
+                cout << " --> (" << tmp->getName() << "," << tmp->getType() << ")";
+                tmp = tmp->getNext();
+            }
+            cout << endl;
+        }
+    }
 };
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+cout << s1.Delete("i") << endl;
+    // cout << s1.Delete("j") << endl;
+    // cout << s1.Delete("k") << endl;
+    // cout << s1.Delete("l") << endl;
+    // cout << s1.Delete("m") << endl;
+    // cout << s1.Delete("n") << endl;
+    // cout << s1.Delete("o") << endl;
+    // cout << s1.Delete("p") << endl;
+    // cout << s1.Delete("q") << endl;
+    // cout << s1.Delete("r") << endl;
+    // cout << s1.Delete("s") << endl;
+    // cout << s1.Delete("t") << endl;
